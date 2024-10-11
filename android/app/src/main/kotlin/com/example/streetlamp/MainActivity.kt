7c2e86293a11a0b92f1f4cc575ac76a41c4b0602
hyooosong <gythd1998@gmail.com>
2024-10-11 09:44:34 +0900

    [feature] init commit

---
+++ android/app/src/main/kotlin/com/example/streetlamp/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.streetlamp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return ScreenUtilInit(
+      designSize: const Size(1280, 720),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      builder: (_, child) {
+        return MaterialApp(
+            debugShowCheckedModeBanner: false,
+            home: child);
+      },
+      child: const MyHomePage(),
+    );
+  }
+}
+
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      body: Center(
+      ),
+    );
+  }
+}
